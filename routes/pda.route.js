4338fe0060204412b1070e21452cf5550bc93a43
Kirtan-lokadiya <kirtanlokadiya998@gmail.com>
2024-11-12 02:07:58 +0530

    get all pda account details in admin side api done

--- routes/pda.route.js
+++ routes/pda.route.js
@@ -7,9 +7,11 @@ const {
   updatePDA,
   deletePDA
 } = require('../controllers/pda.controller');
+const isAdmin = require('../middlewares/isAdmin');
+
 const router = express.Router();
 
-router.get('/', getPDAs);
+router.get('/',isAdmin, getPDAs);
 router.post('/', createPDA);
 router.get('/:id', getPDAById);
 router.put('/:id', updatePDA);
