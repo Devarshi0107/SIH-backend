e95470cff7918e7639982a498cf3ba604efe1697
Kirtan-lokadiya <kirtanlokadiya998@gmail.com>
2024-11-12 02:05:55 +0530

    philatelic item create by post circle api done only by post circle

--- routes/philatelicItem.route.js
+++ routes/philatelicItem.route.js
@@ -1,9 +1,11 @@
 // routes/philatelicItemRoutes.js
 const express = require('express');
 const { getPhilatelicItems, createPhilatelicItem } = require('../controllers/philatelicItem.controller');
+const authenticatePostalCircle = require('../middlewares/authenticatePostalCircle');
+
 const router = express.Router();
 
 router.get('/', getPhilatelicItems);
-router.post('/', createPhilatelicItem);
+router.post('/',authenticatePostalCircle, createPhilatelicItem);
 
 module.exports = router;
