191008bea9d6919f549eb880709a4f7eab0393b1
Kirtan-lokadiya <kirtanlokadiya998@gmail.com>
2024-11-12 20:42:35 +0530

    add wallet balance full api done

--- routes/wallet.route.js
+++ routes/wallet.route.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const walletController = require('../controllers/wallet.controller');
+const authMiddleware = require('../middlewares/authenticateuser');
+
 
 router.post('/create-payment-intent', walletController.createPaymentIntent);
-router.get('/success', walletController.verifyPayment)
+router.get('/success', authMiddleware, walletController.verifyPayment)
 
 module.exports = router;
