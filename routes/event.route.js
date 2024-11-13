c61e1ef2a92698a496c4e8b4dd6f0dfba8ebee94
Kirtan-lokadiya <kirtanlokadiya998@gmail.com>
2024-11-13 23:23:20 +0530

    upcoming event api done

--- routes/event.route.js
+++ routes/event.route.js
@@ -1,18 +1,21 @@
-// routes/eventRoutes.js
 const express = require('express');
 const {
   getEvents,
   createEvent,
   getEventById,
   updateEvent,
-  deleteEvent
+  deleteEvent,
+  getUpcomingEvents // Import the new function
 } = require('../controllers/event.controller');
 const router = express.Router();
 
 router.get('/', getEvents);
+router.get('/upcoming', getUpcomingEvents);
 router.post('/', createEvent);
 router.get('/:id', getEventById);
 router.put('/:id', updateEvent);
 router.delete('/:id', deleteEvent);
+
+// New route for upcoming events
 
 module.exports = router;
