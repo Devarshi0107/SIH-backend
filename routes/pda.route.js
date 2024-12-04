ad60d2ec21a313f24e950630694cf83eeb1276d4
Vasu <129766426+VasuBhalani@users.noreply.github.com>
2024-12-04 21:58:03 +0530

    add Comments in PDA Routes

--- routes/pda.route.js
+++ routes/pda.route.js
@@ -15,11 +15,11 @@ const authMiddleware = require('../middlewares/authenticateuser');
 
 const router = express.Router();
 
-router.get('/',authMiddleware , isAdmin, getPDAs);
-router.get('/pda-accounts', authMiddleware, userPdaDetails);
-router.get('/user/:userId/accounts', authMiddleware, getUserPDAccounts);
-router.post('/',authMiddleware, createPDA);
-router.get('/:id', getPDAById);
+router.get('/',authMiddleware , isAdmin, getPDAs); // all pda account in website access only by admin so not require for authMiddleware
+router.get('/pda-accounts', authMiddleware, userPdaDetails); // it show in logged user profile i.e Person A how much PDA that show on his/her Profile
+router.get('/user/:userId/accounts', authMiddleware, getUserPDAccounts); // specific user mate  
+router.post('/',authMiddleware, createPDA); // Creation of PDA by user
+router.get('/:id', getPDAById); 
 router.put('/:id', updatePDA);
 router.delete('/:id', deletePDA);
   
